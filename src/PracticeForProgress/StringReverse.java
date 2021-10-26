7b3e6ef0709a82cb0c202e9c8b83f5527ec1a5e8
nadiiapiekh <nadiapiekh@gmail.com>
2021-10-26 00:12:59 -0500

    String Reverse

---
+++ src/PracticeForProgress/StringReverse.java
@@ -0,0 +1,15 @@
+package PracticeForProgress;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        System.out.println(reverseString("Hello Java!"));
+    }
+
+    public static String reverseString (String str){
+        String reversed = "";
+         for (int i=str.length()-1; i>=0; i--){
+             reversed +=str.charAt(i);
+         }
+         return reversed;
+    }
+}
