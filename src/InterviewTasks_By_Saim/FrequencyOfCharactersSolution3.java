f6f47052217bf97aa0e146a55f5dac771dc7ecb4
nadiiapiekh <nadiapiekh@gmail.com>
2021-10-30 16:46:54 -0500

    String Frequency of Characters (Solution 3)

--- src/InterviewTasks_By_Saim/FrequencyOfCharactersSolution3.java
+++ src/InterviewTasks_By_Saim/FrequencyOfCharactersSolution3.java
@@ -20,6 +20,7 @@ public class FrequencyOfCharactersSolution3 {
         return result;
     }
 }
+
 /*
 Write a return method that can find the frequency of characters
 Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
