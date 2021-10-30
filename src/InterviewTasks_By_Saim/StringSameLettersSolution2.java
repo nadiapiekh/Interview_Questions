fad8ea77eae76aa4267ba9194f22472142a6d0ba
nadiiapiekh <nadiapiekh@gmail.com>
2021-10-30 16:41:14 -0500

    String Same Letters (Solution 2)

---
+++ src/InterviewTasks_By_Saim/StringSameLettersSolution2.java
@@ -0,0 +1,17 @@
+package InterviewTasks_By_Saim;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class StringSameLettersSolution2 {
+    public static boolean Same(String str1, String str2) {
+        str1 = new TreeSet<String>(Arrays.asList(str1.split(""))).toString();
+        str2 = new TreeSet<String>(Arrays.asList(str2.split(""))).toString();
+        return str1.equals(str2);
+    }
+}
+/*
+Write a return method that check if a string is build out of the same letters as another string.
+Ex:  same("abc",  "cab"); -> true
+same("abc",  "abb"); -> false:
+ */
