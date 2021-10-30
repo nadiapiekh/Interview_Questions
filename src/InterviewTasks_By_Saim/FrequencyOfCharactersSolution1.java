be2897643a2caace211a65860e85eedfe3f1e103
nadiiapiekh <nadiapiekh@gmail.com>
2021-10-30 16:36:45 -0500

    Frequency Of Characters Solution

--- src/InterviewTasks_By_Saim/FrequencyOfCharactersSolution1.java
+++ src/InterviewTasks_By_Saim/FrequencyOfCharactersSolution1.java
@@ -18,3 +18,8 @@ public class FrequencyOfCharactersSolution1 {
         return expectedResult;
     }
 }
+
+/*
+Write a return method that can find the frequency of characters
+Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
--- src/InterviewTasks_By_Saim/FrequencyOfCharactersSolution4.java
+++ src/InterviewTasks_By_Saim/FrequencyOfCharactersSolution4.java
@@ -16,3 +16,7 @@ public class FrequencyOfCharactersSolution4 {
         return result;
     }
 }
+/*
+Write a return method that can find the frequency of characters
+Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
